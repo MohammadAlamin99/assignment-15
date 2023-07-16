88d4e0dd7403b20dea837719102020e80e587c66
Mohammad alamin <alamin.cse.99@gmail.com>
2023-07-16 17:09:47 +0600

    some change js and css

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,51 +1,59 @@
-
 import React, { useState } from 'react';
 import Footer from './Footer';
 
 const Content = () => {
+  const [activity, setActivity] = useState("");
+  const [listData, setListData] = useState([]);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
 
-
-  const [activity, setActivity]=useState("");
-  const [listData, setListData]=useState([]);
-
-  function addActivity(){
-    setListData((listData)=>{
-      const updatedList = ([...listData, activity]);
-      console.log(updatedList)
-      setActivity('')
+  function addActivity() {
+    setListData((listData) => {
+      const updatedList = [...listData, activity];
+      console.log(updatedList);
+      setActivity('');
+      setIsButtonClicked(true); // Set the button click state to true
       return updatedList;
-    })
-
+    });
   }
 
-
-  function removectivity(i){
-    const updatedListData = listData.filter((elem, id)=>{
-        return i!=id;
-    })
-    setListData(updatedListData)
+  function removectivity(i) {
+    const updatedListData = listData.filter((elem, id) => {
+      return i !== id;
+    });
+    setListData(updatedListData);
   }
 
   return (
     <div className='container'>
-      <input type="text" placeholder='Add Your Activity' value={activity} onChange={(e)=>{
-        setActivity(e.target.value)
-      }} />
+      <input
+        type="text"
+        placeholder='Add Your Activity'
+        value={activity}
+        onChange={(e) => {
+          setActivity(e.target.value);
+        }}
+      />
       <button className='btn btn-success' onClick={addActivity}>Add</button>
-      <p className='list-heading'>Here is your list:{")"} </p>
-      {
-        listData!=[] && listData.map((data, i)=>{
-          return(
-            <>
-              <p key={i}>
-                <div className='listData'>{data}</div>
-                <div className='btn-position'><button className='remov btn btn-danger' onClick={()=>{removectivity(i)}}>remove(-)</button></div>
-              </p>
-            </>
-          )
-        })
-      }
-
+      <p className='list-heading'>
+        {isButtonClicked ? 'Here is your to do list:' : 'Enter your to do list'}
+      </p>
+      {listData.length !== 0 && listData.map((data, i) => {
+        return (
+          <div key={i}>
+            <div className='listData'>{data}</div>
+            <div className='btn-position'>
+              <button
+                className='remov btn btn-danger'
+                onClick={() => {
+                  removectivity(i);
+                }}
+              >
+                remove(-)
+              </button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
